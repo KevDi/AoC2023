96faf70e8a7882123c361e50325853d48f02991a
Kevin <kevin.dick91@yahoo.de>
2023-12-02 21:24:01 +0100

    added additional test

--- day02/day02_test.cpp
+++ day02/day02_test.cpp
@@ -13,6 +13,8 @@ TEST(Day02, GameSetsID) {
 
 TEST(Day02, GameSetsBlue) {
     Game game{"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"};
+    Game game_2{"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"};
 
     ASSERT_THAT(game.blue(), Eq(6));
+    ASSERT_THAT(game_2.blue(), Eq(2));
 }
