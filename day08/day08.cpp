383a6baf8c7029a7b1583696818558ed92b8ff4d
Kevin <kevin.dick91@yahoo.de>
2023-12-10 21:02:47 +0100

    Finished Day 8 and 9

--- day08/day08.cpp
+++ day08/day08.cpp
@@ -1,6 +1,90 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <map>
+#include <numeric>
+#include <regex>
 
 int main() {
-    std::cout << "Hello World\n";
-}
+    std::ifstream data{"resources/data.txt"};
+    long long steps = 0;
+    std::map<std::string, std::pair<std::string, std::string>> network_map;
+    std::vector<std::string> starting_nodes;
+    std::string instructions;
+    if (data.is_open()) {
+        std::string line;
+        std::regex network_reg("(\\w+)\\s=\\s\\((\\w+),\\s(\\w+)\\)");
+        std::smatch smatch;
+        std::getline(data, line);
 
+        instructions = line;
+
+        std::getline(data, line);
+        while (std::getline(data, line)) {
+            if (std::regex_match(line, smatch, network_reg)) {
+                if (smatch.size() != 4) {
+                    throw std::invalid_argument{"Wrong input"};
+                }
+
+                network_map[smatch[1].str()] = {smatch[2].str(), smatch[3].str()};
+                if (smatch[1].str().back() == 'A') {
+                    starting_nodes.push_back(smatch[1].str());
+                }
+            }
+        }
+
+        data.close();
+    }
+
+    std::string start = "AAA";
+    for (int i = 0; i <= instructions.size(); i++) {
+        if (start == "ZZZ") {
+            break;
+        }
+
+        if (i == instructions.size()) {
+            i = 0;
+        }
+
+        if (instructions[i] == 'L') {
+            start = network_map[start].first;
+        } else {
+            start = network_map[start].second;
+        }
+        steps++;
+    }
+
+    std::cout << steps << '\n';
+    steps = 0;
+
+    std::vector<long long> stepsToEnd(starting_nodes.size(), 0);
+
+    for (int i = 0; i <= instructions.size(); i++) {
+        steps++;
+        if (i == instructions.size()) {
+            i = 0;
+        }
+
+        for (int j = 0; j < starting_nodes.size(); j++) {
+            if (stepsToEnd[j] > 0LL) {
+                continue;
+            }
+
+            std::string& current = starting_nodes[j];
+            current = instructions[i] == 'L' ? network_map[current].first : network_map[current].second;
+
+            if (current.back() == 'Z') {
+                stepsToEnd[j] = steps;
+            }
+        }
+
+        if (std::all_of(stepsToEnd.begin(), stepsToEnd.end(), [](long long val) { return val > 0LL; })) {
+            break;
+        }
+    }
+
+    auto res = std::accumulate(stepsToEnd.begin(), stepsToEnd.end(), 1LL, [](long long acc, long long val) {
+        return std::lcm(acc, val);
+    });
+    std::cout << res << '\n';
+}
--- day09/day09.cpp
+++ day09/day09.cpp
@@ -1,6 +1,53 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <istream>
+#include <numeric>
+#include <sstream>
+#include <vector>
 
 int main() {
-    std::cout << "Hello World\n";
-}
+    std::ifstream data{"resources/data.txt"};
+    long sum = 0;
+    long sum_part_2 = 0;
+    if (data.is_open()) {
+        std::string line;
+        while (std::getline(data, line)) {
+            std::vector<long> tokens;
 
+            std::string token;
+            std::istringstream tokenStream(line);
+            while (std::getline(tokenStream, token, ' ')) {
+                tokens.push_back(std::stoi(token));
+            }
+
+            std::vector<long> res(tokens.size(), 0);
+            std::vector<long> last_elements{tokens.back()};
+            std::vector<long> first_elements{tokens.front()};
+
+            std::adjacent_difference(tokens.begin(), tokens.end(), res.begin());
+            res.erase(res.begin());
+            last_elements.push_back(res.back());
+            first_elements.push_back(res.front());
+
+            while (!std::all_of(res.begin(), res.end(), [](int ele) { return ele == 0; })) {
+                std::adjacent_difference(res.begin(), res.end(), res.begin());
+                res.erase(res.begin());
+                first_elements.push_back(res.front());
+                last_elements.push_back(res.back());
+            }
+
+            sum += std::accumulate(last_elements.begin(), last_elements.end(), 0L);
+
+            long first_element_res = 0L;
+
+            for (size_t i = first_elements.size() - 1; i > 0; i--) {
+                first_element_res = first_elements[i] - first_element_res;
+            }
+
+            sum_part_2 += first_elements[0] - first_element_res;
+        }
+    }
+    std::cout << sum << '\n';
+    std::cout << sum_part_2 << '\n';
+}
