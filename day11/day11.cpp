2ab1326cc5e063b1310fb56058b1d80fa9376a18
Jan Kevin Dick <Jan.Dick@de.bosch.com>
2023-12-11 13:04:24 +0100

    Finished Day 11

--- day11/day11.cpp
+++ day11/day11.cpp
@@ -6,33 +6,57 @@
 #include <vector>
 
 int main() {
+    long groth = 1000000 - 1;
     std::ifstream data{"resources/data.txt"};
     std::vector<std::string> data_map;
-    std::vector<std::pair<int, int>> galaxies;
+    std::vector<std::pair<long, long>> galaxies;
     std::regex galaxy_reg{"#"};
     std::smatch match;
-    int counter = 0;
+    long counter = 0L;
     if (data.is_open()) {
         std::string line;
 
         while (std::getline(data, line)) {
             std::sregex_token_iterator word_begin{line.begin(), line.end(), galaxy_reg};
             const std::sregex_token_iterator word_end;
+            counter = word_begin == word_end ? counter + groth : counter;
             while (word_begin != word_end) {
-                galaxies.push_back({counter, std::distance(line.begin(), word_begin->first)});
+                galaxies.push_back({counter, std::distance(line.cbegin(), word_begin->first)});
                 word_begin++;
             }
             data_map.push_back(line);
+            counter++;
         }
-
-        // for (auto i = 0; i < data_map[0].size(); i++) {
-        //     for (auto j = 0; j < data_map.size(); j++) {
-        //         if (data_map[j][i] != '.') {
-        //             break;
-        //         }
-        //     }
-        // }
+        counter = 0;
+        for (auto i = 0; i < data_map[0].size(); i++) {
+            bool empty = true;
+            for (auto j = 0; j < data_map.size(); j++) {
+                if (data_map[j][i] != '.') {
+                    empty = false;
+                    break;
+                }
+            }
+            if (empty) {
+                for (auto& galaxy : galaxies) {
+                    if (galaxy.second >= (i + counter)) {
+                        galaxy.second += groth;
+                    }
+                }
+                counter += groth;
+            }
+        }
 
         data.close();
     }
+
+    long long sum = 0;
+
+    for (int i = 0; i < galaxies.size(); i++) {
+        for (int j = i + 1; j < galaxies.size(); j++) {
+            auto dist = galaxies[j].first - galaxies[i].first + std::abs(galaxies[j].second - galaxies[i].second);
+            sum += dist;
+        }
+    }
+
+    std::cout << sum << '\n';
 }
